066c5468ce73111b4cbd3ba8842e8890e5cd323b
yuliiache <ychebarova@gmail.com>
2024-01-04 11:22:30 -0800

    Revert "Feature/T5-89-Input (#8)"
    This reverts commit f11ccf588af400c0eb4fde4e8ddc53144c2bd80b.

--- src/@kolo/pages/UiKit/UiKit.js
+++ src/@kolo/pages/UiKit/UiKit.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import InputGroup from "./components/InputGroup";
-
 const UiKit = () => {
   return (
     <div>
       <h1>UI-kit</h1>
-      <InputGroup/>
     </div>
   );
 };
