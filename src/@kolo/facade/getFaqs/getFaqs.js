9dcfb1c8b451810f2b8cd12131955a369a189443
Sofiia Ruban <37212452+sofiiaruban@users.noreply.github.com>
2024-01-22 14:35:28 +0200

    Revert "T5-104: Add getFaqs facade" (#41)

--- src/@kolo/facade/getFaqs/getFaqs.js
+++
@@ -1,7 +0,0 @@
-import {httpService} from '@kolo/services/httpService/httpService';
-
-import {API_ENDPOINTS} from '../endpoints';
-
-const getFaqs = () => httpService.get(API_ENDPOINTS.FAQS);
-
-export default getFaqs;
