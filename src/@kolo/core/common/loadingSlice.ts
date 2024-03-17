f8eeea956de033f46af7abc81a72c12583a7caa8
Vokoloven <48805990+Vokoloven@users.noreply.github.com>
2024-03-18 00:02:39 +0200

    feature/T5-130-loading-reducer (#72)
    * T5-130-loading-reducer:
    
    -Created loading reducer
    -Put into rootReducer
    
    * chore:
    
    -Added slice types
    
    * fix:
    
    -Fixed according sample

---
+++ src/@kolo/core/common/loadingSlice.ts
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {PayloadAction} from '@reduxjs/toolkit';
+
+import {LOADING_KEY} from './constants';
+
+interface LoadingState {
+  isLoading: boolean;
+}
+
+const initialState: LoadingState = {
+  isLoading: false,
+} as const;
+
+const loading = createSlice({
+  name: LOADING_KEY,
+  initialState,
+  reducers: {
+    setLoading: (state: LoadingState, action: PayloadAction<boolean>): LoadingState => ({
+      ...state,
+      isLoading: action.payload,
+    }),
+  },
+});
+
+const loadingReducer = loading.reducer;
+
+export const {setLoading} = loading.actions;
+
+export default loadingReducer;
--- src/@kolo/core/reducers/getReducers.js
+++ src/@kolo/core/reducers/getReducers.js
@@ -1,5 +1,5 @@
-import { FEEDBACKS_KEY } from '@kolo/pages/Home/components/Feedbacks/constants';
-import feedbacksReducer from '@kolo/pages/Home/components/Feedbacks/feedbacksSlice'
+import {FEEDBACKS_KEY} from '@kolo/pages/Home/components/Feedbacks/constants';
+import feedbacksReducer from '@kolo/pages/Home/components/Feedbacks/feedbacksSlice';
 import {MONTH_PACK_KEY} from '@kolo/pages/Home/components/MonthPack/constants';
 import monthPackReducer from '@kolo/pages/Home/components/MonthPack/monthPackSlice';
 import {LOGIN_USER_KEY} from '@kolo/pages/LoginPage/constants';
@@ -8,12 +8,16 @@ import pricingReducer, {PRICING_KEY} from '@kolo/pages/PricingPage/components/Pr
 import {PRICING_FAQS_KEY} from '@kolo/pages/PricingPage/components/PricingFAQs/constants';
 import pricingFAQsReducer from '@kolo/pages/PricingPage/components/PricingFAQs/slice/pricingFAQsSlice';
 
+import {LOADING_KEY} from '../common/constants';
+import loadingReducer from '../common/loadingSlice';
+
 const reducers = {
   [MONTH_PACK_KEY]: monthPackReducer,
   [FEEDBACKS_KEY]: feedbacksReducer,
   [PRICING_FAQS_KEY]: pricingFAQsReducer,
   [LOGIN_USER_KEY]: loginReducer,
   [PRICING_KEY]: pricingReducer,
+  [LOADING_KEY]: loadingReducer,
   // ...
 };
 
